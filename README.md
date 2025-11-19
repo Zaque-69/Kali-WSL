e31f4098fb2278553bc429af38b082aa4bc02fcf
Z4que <114612941+Zaque-69@users.noreply.github.com>
2025-11-19 14:49:33 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# Kali-WSL minimal config
+
+One-liner shell script that will setup your WSL.
+
+## Usage
+
+Clone and run the shell:
+
+```bash
+chmod +x run.sh & sudo sh ./run.sh
+```
---
+++ run.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+set -e
+
+# Update the system
+sudo apt update && \
+sudo apt upgrade -y && \
+
+# Cybersecurity packages
+sudo apt install \
+  fish \
+  aircrack-ng \
+  apktool \
+  binwalk \
+  curl \
+  dex2jar \
+  exiftool \
+  file \
+  gdb \
+  git \
+  jd-gui \
+  john \
+  lm-sensors \
+  nmap \
+  p7zip \
+  pip \
+  pngcheck \
+  pngcrush \
+  sherlock \
+  steghide \
+  wget -y && \
+
+# Python3 libraries
+pip3 install customtkinter beautifulsoup4 librosa pillow requests setuptools --break-system-packages
+
+# Ruby packages
+sudo apt install -y ruby ruby-dev
+sudo gem install zsteg
+
+# Terminal configuration
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+fisher install IlanCosman/tide
+tide configure --auto --style=Lean --prompt_colors='True color' --show_time='24-hour format' --lean_prompt_height='Two lines' --prompt_connection=Disconnected --prompt_spacing=Sparse --icons='Few icons' --transient=Yes
+
+# Github repos
+git clone https://github.com/dylanaraps/neofetch
+cd neofetch
+sudo make install
+cd ..
+sudo rm -rf neofetch
+
+# Others
+rm ~/.bashrc && cp .bashrc ~/
